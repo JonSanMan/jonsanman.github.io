c9e6ede7abf2098637f9600e0dd6708624afb719
Jon Sanchez <jon@jon-s.com>
2023-03-14 16:24:31 -0700

    Come to senses and switch to Tab indent

--- scripts.js
+++ scripts.js
@@ -6,13 +6,13 @@ document.addEventListener("DOMContentLoaded", setdomvars);
 window.addEventListener("scroll", checkypos);
 
 function checkypos() {
-  if (window.pageYOffset > (visualViewport.height * 0.35)) {
-    banner.classList.add("sticky");
-  } else {
-    banner.classList.remove("sticky");
-  }
+	if (window.pageYOffset > (visualViewport.height * 0.35)) {
+		banner.classList.add("sticky");
+	} else {
+		banner.classList.remove("sticky");
+	}
 }
 
 function setdomvars() {
-  banner = document.getElementById("banner");
+	banner = document.getElementById("banner");
 }
